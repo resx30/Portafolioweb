e6fe101107fec2087123245f6f5d50bf0c3b1930
resx30 <140671738+resx30@users.noreply.github.com>
2025-07-04 07:52:55 -0500

    Add files via upload

--- Portafolio/script.js
+++ Portafolio/script.js
@@ -38,3 +38,12 @@ botones.forEach(boton => {
     });
   });
 });
+
+const header = document.querySelector('.main-header')
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 10) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
